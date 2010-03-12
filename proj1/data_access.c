342acc4d6cf12fd8d18a4deab4c21c0de1ecdef9
felipe.eltermann <felipe.eltermann@2732cb02-2d7a-11df-b28a-19025b8c0740>
2010-03-12 03:13:23 +0000

    Documentação referente ao formato do registro no arquivo

---
+++ proj1/data_access.c
@@ -0,0 +1,26 @@
+
+int da_string_from_file_to_filme(filme *f_ret, char *f_str) {
+	/* Entrada: string crua lida do arquivo, referente a um registro */
+	/* Saída: - setup da struct filme passada por referência 
+						- valor numérico para erros */
+	
+
+	/* Formato do registro no arquivo */
+	/* Como há atributos de texto, é difícil manter os registros com tamanho constante no arq */
+	/* Assim, uma ideia é usar separadores entre os atributos, assim como manter o tamanho do registro */
+	/* 
+		int tam_total_do_reg (incluindo todos os separadores do registro)
+		int id, string titulo, string sinopse, string sala, string horarios
+		Separador: @
+		Ex: 75@1@Rei Leão@Filme assim assim assado@Kinoplex - sala 10@12h40, 15h, 17h20@
+		(contando tudo, incluindo os caracteres de tam_total_do_reg e os separadores)
+
+		Esse esquema do número de caracteres num campo é pra possibilitar a busca entre os registros.
+	 */
+
+
+
+
+
+	return(0);
+}
