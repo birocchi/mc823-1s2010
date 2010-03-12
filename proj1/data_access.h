04533a4395305abe38f1f128f0fd5d019297ba8f
felipe.eltermann <felipe.eltermann@2732cb02-2d7a-11df-b28a-19025b8c0740>
2010-03-12 02:41:17 +0000

    Início do header para as funções de acesso aos dados (usadas pelo servidor e possivelmente o cliente)

---
+++ proj1/data_access.h
@@ -0,0 +1,19 @@
+typedef struct {
+	int id;
+	char *titulo;
+	char *sinopse;
+	char *sala;
+	char *horarios;
+
+	struct filme *prox_filme; /* NULL caso seja um único filme ou o último da lista */
+} filme;
+
+/* 'da' refere-se a data access (para facilitar depois pra chamar as funções) */
+
+/* Função que retorna um filme a partir de um id */
+int da_get_filme_by_id(filme *f_ret, int id);
+
+/* Função responsável por fazer um parse da string lida do arquivo para um filme */
+int da_string_from_file_to_filme(filme *f_ret, char *f_str);
+
+
