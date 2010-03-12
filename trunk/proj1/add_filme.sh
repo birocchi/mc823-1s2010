b838c25303fd2e2b1cecb7d4f93c80f109781dc0
felipe.eltermann <felipe.eltermann@2732cb02-2d7a-11df-b28a-19025b8c0740>
2010-03-12 19:06:49 +0000

    Adicionado script de alimentação do banco de dados. Para rodar: $ sh add_filme.sh e seguir as instruções

---
+++ trunk/proj1/add_filme.sh
@@ -0,0 +1,15 @@
+echo "Cadastro de novo filme!"
+echo -n "Id: "; read id
+echo -n "Titulo: "; read titulo
+echo -n "Sinopse: "; read sinopse
+echo -n "Sala: "; read sala
+echo -n "Horários: "; read horarios
+
+tam_substr=$(echo -n $id\@$titulo\@$sinopse\@$sala\@$horarios\@ | wc -c)
+
+# tam_reg deve ser tam_substr + 3~4 (digitos+@)
+tam_reg=$(echo -n $tam_substr\@$id\@$titulo\@$sinopse\@$sala\@$horarios\@ | wc -c)
+
+# finalmente, manda os valores para o arquivo
+echo -n $tam_reg\@$id\@$titulo\@$sinopse\@$sala\@$horarios\@ >> filmes.dat
+
