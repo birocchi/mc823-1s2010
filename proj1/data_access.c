01ab6f284921a7bb43ef7dcd511d1820a70ab91c
felipe.eltermann <felipe.eltermann@2732cb02-2d7a-11df-b28a-19025b8c0740>
2010-03-12 18:42:20 +0000

    Corrigindo algumas coisas pequenas

--- proj1/data_access.c
+++ proj1/data_access.c
@@ -3,7 +3,7 @@
 #include "data_access.h"
 
 int da_str_to_filme (filme *f_ret, int *tam_reg, char *f_str) {
-	/* Entrada: string crua lida do arquivo, referente a um registro */
+	/* Entrada: string crua lida do arquivo, a partir do início de um registro */
 	/* Saída: - setup da struct filme passada por referência 
 		        - tamanho do registro no arquivo
 						- valor numérico para erros */
@@ -21,11 +21,6 @@ int da_str_to_filme (filme *f_ret, int *tam_reg, char *f_str) {
 
 		Esse esquema do número de caracteres num campo é pra possibilitar a busca entre os registros.
 	 */
-
-	/*  Importante!
-		 Essa função só faz o processamento (parse); ela considera que f_str é válida
-		 e que a struct f_ret já tem memória alocada para as strings 
-	*/
 
 	char str[TAM_MAX_ATR]; /* string fixa bem grande pra comportar qq atributo */
 
@@ -52,32 +47,75 @@ int da_str_to_filme (filme *f_ret, int *tam_reg, char *f_str) {
 	/* titulo */
 	i++; j = 0;
 	while(f_str[i]!='@') {
-		(*f_ret).titulo[j] = f_str[i];
+		str[j] = f_str[i];
 		i++; j++;
 	}
-	(*f_ret).titulo[j] = '\0';
+	str[j] = '\0';
+	/* aloca a string dinamicamente */
+	(*f_ret).titulo = (char *)malloc(j*sizeof(char));
+	sprintf((*f_ret).titulo, "%s", str);
+
+	/* Blocos de código similares ao de cima, só que comprimidos  */
+	/* sinopse */
+	i++; j = 0;
+	while(f_str[i]!='@') { str[j] = f_str[i];	i++; j++;	} str[j] = '\0';
+	(*f_ret).sinopse = (char *)malloc(j*sizeof(char));
+	sprintf((*f_ret).sinopse, "%s", str);
+
+	/* sala */
+	i++; j = 0;
+	while(f_str[i]!='@') { str[j] = f_str[i]; i++; j++; } str[j] = '\0';
+	(*f_ret).sala = (char *) malloc(j*sizeof(char));
+	sprintf((*f_ret).sala, "%s", str);
+
+	/* horarios */
+	i++; j = 0;
+	while(f_str[i]!='@') { str[j] = f_str[i]; i++; j++; } str[j] = '\0';
+	(*f_ret).horarios = (char *) malloc(j*sizeof(char));
+	sprintf((*f_ret).horarios, "%s", str);
+
+	/* next - lembra dele? :) */
+	(*f_ret).prox_filme = NULL;
 
 	return(0);
 }
 
+
+void da_print_infos(filme *f) {
+	printf("Id: %d\n", (*f).id);
+	printf("Titulo: %s\n", (*f).titulo);
+	printf("Sinopse: %s\n", (*f).sinopse);
+	printf("Sala: %s\n", (*f).sala);
+	printf("Horários: %s\n", (*f).horarios);
+	return;
+}
+
+void da_free_strs(filme *f) {
+	free((*f).titulo);
+	free((*f).sinopse);
+	free((*f).sala);
+	free((*f).horarios);
+	return;
+}
 
 int main() {
 
 	filme f;
 
 	char *teste;
-	teste = "800@12@Rei Leão@";
+	teste = "800@12@Rei Leão@Bla@Sala2@10h@";
 
 	int tam_reg;
 
 	if(da_str_to_filme(&f, &tam_reg, teste)==0) {
 		printf("Tamanho do reg no arq: %d\n", tam_reg);
-		printf("Id: %d\n", f.id);
-		printf("Titulo: %s\n", f.titulo);
+		da_print_infos(&f);
 	}
 	else {
 		printf("Ocorreu algum problema...\n\n");
 	}
 
+	da_free_strs(&f);
+	
 	return(0);
 }
