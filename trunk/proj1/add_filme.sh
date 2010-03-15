a64b18b311336c0aa5a511772df4173c65b91fa8
felipe.eltermann <felipe.eltermann@2732cb02-2d7a-11df-b28a-19025b8c0740>
2010-03-15 17:05:20 +0000

    Corrigido bug na inserção de filmes. Se a substring tivesse tamanho 98, o tamanho de sua representação seria 2, que somando dá 100. Porém, ao concatenar 100 com a substring, o tamanho da string final fica 101 ao invés de 100. Esse erro sempre acontecia a cada virada pra 10, 100, 1000...

--- trunk/proj1/add_filme.sh
+++ trunk/proj1/add_filme.sh
@@ -10,6 +10,15 @@ tam_substr=$(echo -n $id\@$titulo\@$sinopse\@$sala\@$horarios\@ | wc -c)
 # tam_reg deve ser tam_substr + 3~4 (digitos+@)
 tam_reg=$(echo -n $tam_substr\@$id\@$titulo\@$sinopse\@$sala\@$horarios\@ | wc -c)
 
+# expressão regular ^10*$ casa com qq seq começando com 1 seguida de zeros
+if [ $tam_reg = $(echo $tam_reg | grep ^10*$) ]
+then
+		echo 'Entrou no caso da exceção da virada 10/100/1000'
+		tam_reg=$((tam_reg + 1))
+else
+		echo 'Não entrou na exceção.'
+fi
+
 # finalmente, manda os valores para o arquivo
 echo -n $tam_reg\@$id\@$titulo\@$sinopse\@$sala\@$horarios\@ >> filmes.dat
 
