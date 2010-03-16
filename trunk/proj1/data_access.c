bba147e5f5bc05bbd3865d227ff19a4ef87cea4d
felipe.eltermann <felipe.eltermann@2732cb02-2d7a-11df-b28a-19025b8c0740>
2010-03-16 02:44:51 +0000

    Implementação da função que retorna um filme a partir do seu id, acessando o arquivo dos registros

--- trunk/proj1/data_access.c
+++ trunk/proj1/data_access.c
@@ -3,119 +3,149 @@
 #include "data_access.h"
 
 int da_str_to_filme (filme *f_ret, int *tam_reg, char *f_str) {
-	/* Entrada: string crua lida do arquivo, a partir do início de um registro */
-	/* Saída: - setup da struct filme passada por referência 
-		        - tamanho do registro no arquivo
-						- valor numérico para erros */
+  /* Entrada: string crua lida do arquivo, a partir do início de um registro */
+  /* Saída: - setup da struct filme passada por referência 
+     - tamanho do registro no arquivo
+     - valor numérico para erros */
 	
 
-	/* Formato do registro no arquivo */
-	/* Como há atributos de texto, é difícil manter os registros com tamanho constante no arq */
-	/* Assim, uma ideia é usar separadores entre os atributos, assim como manter o tamanho do registro */
-	/* 
-		int tam_total_do_reg (incluindo todos os separadores do registro)
-		int id, string titulo, string sinopse, string sala, string horarios
-		Separador: @
-		Ex: 75@1@Rei Leão@Filme assim assim assado@Kinoplex - sala 10@12h40, 15h, 17h20@
-		(contando tudo, incluindo os caracteres de tam_total_do_reg e os separadores)
+  /* Formato do registro no arquivo */
+  /* Como há atributos de texto, é difícil manter os registros com tamanho constante no arq */
+  /* Assim, uma ideia é usar separadores entre os atributos, assim como manter o tamanho do registro */
+  /* 
+     int tam_total_do_reg (incluindo todos os separadores do registro)
+     int id, string titulo, string sinopse, string sala, string horarios
+     Separador: @
+     Ex: 75@1@Rei Leão@Filme assim assim assado@Kinoplex - sala 10@12h40, 15h, 17h20@
+     (contando tudo, incluindo os caracteres de tam_total_do_reg e os separadores)
 
-		Esse esquema do número de caracteres num campo é pra possibilitar a busca entre os registros.
-	 */
+     Esse esquema do número de caracteres num campo é pra possibilitar a busca entre os registros.
+  */
 
-	char str[TAM_MAX_ATR]; /* string fixa bem grande pra comportar qq atributo */
+  char str[TAM_MAX_ATR]; /* string fixa bem grande pra comportar qq atributo */
 
-	int i = 0; /* índice de acesso de f_str */
-	int j = 0; /* indice para montagem da string  */
+  int i = 0; /* índice de acesso de f_str */
+  int j = 0; /* indice para montagem da string  */
 
-	/* tamanho do registro */
-	while(f_str[i]!='@') {
-		str[j] = f_str[i];
-		i++; j++;
-	}
-	str[j] = '\0';
- 	*tam_reg = atoi(str);
+  /* tamanho do registro */
+  while(f_str[i]!='@') {
+    str[j] = f_str[i];
+    i++; j++;
+  }
+  str[j] = '\0';
+  *tam_reg = atoi(str);
 
-	/* id */
-	i++; j = 0;
-	while(f_str[i]!='@') {
-		str[j] = f_str[i];
-		i++; j++;
-	}
-	str[j] = '\0';
-	(*f_ret).id = atoi(str);
+  /* id */
+  i++; j = 0;
+  while(f_str[i]!='@') {
+    str[j] = f_str[i];
+    i++; j++;
+  }
+  str[j] = '\0';
+  (*f_ret).id = atoi(str);
 
-	/* titulo */
-	i++; j = 0;
-	while(f_str[i]!='@') {
-		str[j] = f_str[i];
-		i++; j++;
-	}
-	str[j] = '\0';
-	/* aloca a string dinamicamente */
-	(*f_ret).titulo = (char *)malloc(j*sizeof(char));
-	sprintf((*f_ret).titulo, "%s", str);
+  /* titulo */
+  i++; j = 0;
+  while(f_str[i]!='@') {
+    str[j] = f_str[i];
+    i++; j++;
+  }
+  str[j] = '\0';
+  /* aloca a string dinamicamente */
+  (*f_ret).titulo = (char *)malloc(j*sizeof(char));
+  sprintf((*f_ret).titulo, "%s", str);
 
-	/* Blocos de código similares ao de cima, só que comprimidos  */
-	/* sinopse */
-	i++; j = 0;
-	while(f_str[i]!='@') { str[j] = f_str[i];	i++; j++;	} str[j] = '\0';
-	(*f_ret).sinopse = (char *)malloc(j*sizeof(char));
-	sprintf((*f_ret).sinopse, "%s", str);
+  /* Blocos de código similares ao de cima, só que comprimidos  */
+  /* sinopse */
+  i++; j = 0;
+  while(f_str[i]!='@') { str[j] = f_str[i];	i++; j++;	} str[j] = '\0';
+  (*f_ret).sinopse = (char *)malloc(j*sizeof(char));
+  sprintf((*f_ret).sinopse, "%s", str);
 
-	/* sala */
-	i++; j = 0;
-	while(f_str[i]!='@') { str[j] = f_str[i]; i++; j++; } str[j] = '\0';
-	(*f_ret).sala = (char *) malloc(j*sizeof(char));
-	sprintf((*f_ret).sala, "%s", str);
+  /* sala */
+  i++; j = 0;
+  while(f_str[i]!='@') { str[j] = f_str[i]; i++; j++; } str[j] = '\0';
+  (*f_ret).sala = (char *) malloc(j*sizeof(char));
+  sprintf((*f_ret).sala, "%s", str);
 
-	/* horarios */
-	i++; j = 0;
-	while(f_str[i]!='@') { str[j] = f_str[i]; i++; j++; } str[j] = '\0';
-	(*f_ret).horarios = (char *) malloc(j*sizeof(char));
-	sprintf((*f_ret).horarios, "%s", str);
+  /* horarios */
+  i++; j = 0;
+  while(f_str[i]!='@') { str[j] = f_str[i]; i++; j++; } str[j] = '\0';
+  (*f_ret).horarios = (char *) malloc(j*sizeof(char));
+  sprintf((*f_ret).horarios, "%s", str);
 
-	/* next - lembra dele? :) */
-	(*f_ret).prox_filme = NULL;
+  /* next - lembra dele? :) */
+  (*f_ret).prox_filme = NULL;
 
-	return(0);
+  return(0);
 }
 
 
 void da_print_infos(filme *f) {
-	printf("Id: %d\n", (*f).id);
-	printf("Titulo: %s\n", (*f).titulo);
-	printf("Sinopse: %s\n", (*f).sinopse);
-	printf("Sala: %s\n", (*f).sala);
-	printf("Horários: %s\n", (*f).horarios);
-	return;
+  printf("Id: %d\n", (*f).id);
+  printf("Titulo: %s\n", (*f).titulo);
+  printf("Sinopse: %s\n", (*f).sinopse);
+  printf("Sala: %s\n", (*f).sala);
+  printf("Horários: %s\n", (*f).horarios);
+  return;
 }
 
 void da_free_strs(filme *f) {
-	free((*f).titulo);
-	free((*f).sinopse);
-	free((*f).sala);
-	free((*f).horarios);
-	return;
+  free((*f).titulo);
+  free((*f).sinopse);
+  free((*f).sala);
+  free((*f).horarios);
+  return;
+}
+
+int da_get_filme_by_id(filme *f_ret, int id) {
+
+  /* Função responsável por acessar o arquivo dos registros,
+   buscar o filme com o id igual ao passado como argumento,
+   e retornar o resultado da busca
+   Saídas: 0 *f_ret aponta p/ o filme encontrado
+           1 - código de retorno que indica que nada foi encontrado
+  */
+
+  filme filme;
+  int tam_reg, id_reg;
+  long int cursor = 0; /* indice de leitura do arquivo */
+  char buffer[TAM_MAX_REG]; /* 1kB */
+  FILE *arq;
+
+  arq = fopen("filmes.dat", "r");
+
+  while(fscanf(arq, "%d@%d@", &tam_reg, &id_reg) != EOF) {
+    /* registro encontrado */
+    if(id_reg == id) {
+      fseek(arq, cursor, SEEK_SET);
+      fgets(buffer, TAM_MAX_REG, arq);
+      da_str_to_filme(&filme, &tam_reg, buffer);
+      *f_ret = filme;
+      fclose(arq);
+      return(0);
+    } else {
+      cursor += tam_reg;
+      fseek(arq, cursor, SEEK_SET);
+    }
+  } /* só vai sair do while se não encontrar o filme */
+
+  fclose(arq);
+  
+  return(1);
 }
 
 int main() {
 
-	filme f;
+  filme f;
+  int id = 1;
 
-	char *teste;
-	teste = "800@12@Rei Leão@Bla@Sala2@10h@";
+  if((da_get_filme_by_id(&f, id)) == 0) {
+    da_print_infos(&f);
+    da_free_strs(&f);
+  } else {
+    printf("Filme não encontrado!\n");
+  }
 
-	int tam_reg;
-
-	if(da_str_to_filme(&f, &tam_reg, teste)==0) {
-		printf("Tamanho do reg no arq: %d\n", tam_reg);
-		da_print_infos(&f);
-	}
-	else {
-		printf("Ocorreu algum problema...\n\n");
-	}
-
-	da_free_strs(&f);
-	
-	return(0);
+  return(0);
 }
