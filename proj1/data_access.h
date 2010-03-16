4095eeb5bacc27ee52d6165f9f6bad02e03f5e00
felipe.eltermann <felipe.eltermann@2732cb02-2d7a-11df-b28a-19025b8c0740>
2010-03-16 02:44:51 +0000

    Implementação da função que retorna um filme a partir do seu id, acessando o arquivo dos registros

--- proj1/data_access.h
+++ proj1/data_access.h
@@ -1,3 +1,4 @@
+#define TAM_MAX_REG 1024 /* 1kB */
 #define TAM_MAX_TIT 30
 #define TAM_MAX_SIN 900
 #define TAM_MAX_SALA 20
@@ -5,6 +6,8 @@
 #define INT 10 /* id ou tamanho do registro */
 
 #define TAM_MAX_ATR 900
+
+#define TAM_REG_ID 20 /* tamanho max dos digitos do tamanho do registro e do id */
 
 typedef struct {
 	int id;
@@ -18,9 +21,6 @@ typedef struct {
 
 /* 'da' refere-se a data access (para facilitar depois pra chamar as funções) */
 
-/* Função que retorna um filme a partir de um id */
-int da_get_filme_by_id(filme *f_ret, int id);
-
 /* Função responsável por fazer um parse da string lida do arquivo para um filme */
 int da_str_to_filme(filme *f_ret, int *tam_reg, char *f_str);
 
@@ -30,5 +30,6 @@ void da_print_infos(filme *f);
 /* Libera as strings alocadas dinamicamente */
 void da_free_strs(filme *f);
 
-/* Retorna um filme a partir do seu id */
-int da_get_filme_by_id(int id, filme *f);
+/* Função que retorna um filme a partir de um id */
+int da_get_filme_by_id(filme *f_ret, int id);
+
