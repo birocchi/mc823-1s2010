6c374686db8f1b0044331942efac5abc4985741a
felipe.eltermann <felipe.eltermann@2732cb02-2d7a-11df-b28a-19025b8c0740>
2010-03-18 17:36:53 +0000

    Adicionados atributos número de avaliações(int) e média(float). Detalhe: em arquivo, a média terá tamanho FIXO, indo de 0 a 100, com 2 dígitos decimais.

--- trunk/proj1/add_filme.sh
+++ trunk/proj1/add_filme.sh
@@ -5,10 +5,10 @@ echo -n "Sinopse: "; read sinopse
 echo -n "Sala: "; read sala
 echo -n "Horários: "; read horarios
 
-tam_substr=$(echo -n $id\@$titulo\@$sinopse\@$sala\@$horarios\@ | wc -c)
+tam_substr=$(echo -n $id\@0\@000000\@$titulo\@$sinopse\@$sala\@$horarios\@ | wc -c)
 
 # tam_reg deve ser tam_substr + 3~4 (digitos+@)
-tam_reg=$(echo -n $tam_substr\@$id\@$titulo\@$sinopse\@$sala\@$horarios\@ | wc -c)
+tam_reg=$(echo -n $tam_substr\@$id\@0\@000000\@$titulo\@$sinopse\@$sala\@$horarios\@ | wc -c)
 
 # expressão regular ^10*$ casa com qq seq começando com 1 seguida de zeros
 if [ $tam_reg = $(echo $tam_reg | grep ^10*$) ]
@@ -20,6 +20,6 @@ else
 fi
 
 # finalmente, manda os valores para o arquivo
-echo -n $tam_reg\@$id\@$titulo\@$sinopse\@$sala\@$horarios\@ >> filmes.dat
+echo -n $tam_reg\@$id\@0\@000000\@$titulo\@$sinopse\@$sala\@$horarios\@ >> filmes.dat
 
 echo "Registro adicionado com sucesso!"
