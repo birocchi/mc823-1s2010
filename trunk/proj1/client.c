b0c37d87eb1ea75b8400072cd879181625df5991
felipe.eltermann <felipe.eltermann@2732cb02-2d7a-11df-b28a-19025b8c0740>
2010-03-23 15:42:32 +0000

    Makefile e teste para compilação e inicialização dos programas. Sim, apesar de estar tudo no mesmo diretório (pq são poucos códigos-fonte) são dois programas distintos! É importante lembrar disso...

---
+++ trunk/proj1/client.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main() {
+
+	printf("Olá! Eu sou o programa cliente.\n");
+
+	return(0);
+
+}
---
+++ trunk/proj1/server.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main() {
+
+	printf("Saudações, sou o programa servidor.\n");
+
+	return(0);
+
+}
