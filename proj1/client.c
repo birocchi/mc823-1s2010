ce255d44fa10cf8a204cefa03f3fb52c1c511680
felipe.eltermann <felipe.eltermann@2732cb02-2d7a-11df-b28a-19025b8c0740>
2010-03-30 22:01:06 +0000

    Parte funcional do read_option() implementada. Fiz alguns testes e está ok...

--- proj1/client.c
+++ proj1/client.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "data_access.h"
 
 #define SAIR 's'
@@ -9,13 +10,36 @@
 #define REG_MEDIA 'e'
 #define REG_AVALIAR 'f'
 
+#define TRUE 1
+
 char read_option() {
-	/* considera os possiveis erros e só sai quando o usuario
-	 digitar um caractere válido */
+  /* considera os possiveis erros e só sai quando o usuario
+     digitar um caractere válido */
 
-	/* TODO */
+  char c, aux;
 
-	return('s');
+  while(TRUE) {
+
+    /* Mensagem com as opções... */
+    system("clear");
+    printf("Mensagem com as opções...\n");
+    printf("Mensagem com as opções...\n");
+    printf("Mensagem com as opções...\n");
+    printf("Mensagem com as opções...\n");
+
+    c = getchar(); aux = getchar();
+    if((c==SAIR || c==LISTAR_TODOS_COMPLETO || c==LISTAR_TODOS ||
+	c==REG_COMPLETO || c==REG_SINOPSE || c==REG_MEDIA ||
+	c==REG_AVALIAR) && aux=='\n') {
+      /* se a opcao lida é válida e o próximo caractere foi Enter, 
+	 retorna o caractere válido digitado*/
+      return(c);
+    } else {
+      /* caso contrário, 'come' todo o resto da linha e volta às msgs */
+      if(aux != '\n') { while(getchar()!='\n'); }
+    }
+  } /* fim do while */
+
 }
 
 
@@ -23,41 +47,41 @@ char read_option() {
 /******[inicio] Funções que implementam os casos de uso  ******/
 /**************************************************************/
 void client_lista_todos_completo() {
-	/* faz uma consulta ao servidor, coletando TODAS as infos de 
-		 TODOS os filmes */
+  /* faz uma consulta ao servidor, coletando TODAS as infos de 
+     TODOS os filmes */
 
-	/* para cada filme f, chama da_print_full_info(f) */
+  /* para cada filme f, chama da_print_full_info(f) */
 
-	/* TODO */
-	return;
+  /* TODO */
+  return;
 }
 
 void client_lista_todos() {
-	/* também pega todas as infos de todos os filmes */
+  /* também pega todas as infos de todos os filmes */
 
-	/* para cada filme f, chama da_print_parcial(f) */
-	/* TODO */
-	return;
+  /* para cada filme f, chama da_print_parcial(f) */
+  /* TODO */
+  return;
 }
 
 void client_reg_completo() {
-	/* TODO */
-	return;
+  /* TODO */
+  return;
 }
 
 void client_reg_sinopse() {
-	/* TODO */
-	return;
+  /* TODO */
+  return;
 }
 
 void client_reg_media() {
-	/* TODO */
-	return;
+  /* TODO */
+  return;
 }
 
 void client_reg_avalia() {
-	/* TODO */
-	return;
+  /* TODO */
+  return;
 }
 
 /**************************************************************/
@@ -68,43 +92,43 @@ void client_reg_avalia() {
 
 int main() {
 
-	char c;
+  char c;
 
-	c = read_option(); 
+  c = read_option(); 
 
-	while(c != SAIR) {
+  while(c != SAIR) {
 
-		switch(c) {
+    switch(c) {
 
-		case LISTAR_TODOS_COMPLETO:
-			client_lista_todos_completo();
-			break;
-		case LISTAR_TODOS:
-			client_lista_todos();
-			break;
-		case REG_COMPLETO:
-			client_reg_completo();
-			break;
-		case REG_SINOPSE:
-			client_reg_sinopse();
-			break;
-		case REG_MEDIA:
-			client_reg_media();
-			break;
-		case REG_AVALIAR:
-			client_reg_avalia();
-			break;
+    case LISTAR_TODOS_COMPLETO:
+      client_lista_todos_completo();
+      break;
+    case LISTAR_TODOS:
+      client_lista_todos();
+      break;
+    case REG_COMPLETO:
+      client_reg_completo();
+      break;
+    case REG_SINOPSE:
+      client_reg_sinopse();
+      break;
+    case REG_MEDIA:
+      client_reg_media();
+      break;
+    case REG_AVALIAR:
+      client_reg_avalia();
+      break;
 
-		default:
-			/* teoricamente, nunca é pra entrar aqui, pois c já
-			 foi filtrado em read_option */
-			c = read_option();
-			break;
-		}
+    default:
+      /* teoricamente, nunca é pra entrar aqui, pois c já
+	 foi filtrado em read_option */
+      c = read_option();
+      break;
+    }
 
-		c = read_option();
-	}
+    c = read_option();
+  }
 	
-	return(0);
+  return(0);
 
 }
