c7f916a1e172dbe071ebaffe058ee49c35e55321
felipe.eltermann <felipe.eltermann@2732cb02-2d7a-11df-b28a-19025b8c0740>
2010-04-19 18:29:02 +0000

    Adicionado include para usar a função close(), tirando os Warnings da compilação.

--- proj1/client.c
+++ proj1/client.c
@@ -9,6 +9,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <unistd.h>
 
 #define PORTA_SERVIDOR 3490  // A porta em que o servidor escuta
 
@@ -50,10 +51,10 @@ char read_option() {
 
     c = getchar(); aux = getchar();
     if((c==SAIR || c==LISTAR_TODOS_COMPLETO || c==LISTAR_TODOS ||
-	c==REG_COMPLETO || c==REG_SINOPSE || c==REG_MEDIA ||
-	c==REG_AVALIAR) && aux=='\n') {
+				c==REG_COMPLETO || c==REG_SINOPSE || c==REG_MEDIA ||
+				c==REG_AVALIAR) && aux=='\n') {
       /* se a opcao lida é válida e o próximo caractere foi Enter, 
-	 retorna o caractere válido digitado*/
+				 retorna o caractere válido digitado*/
       return(c);
     } else {
       /* caso contrário, 'come' todo o resto da linha e volta às msgs */
@@ -112,56 +113,57 @@ void client_reg_avalia() {
 
 
 int main(int argc, char** argv) {
-
+	
+  //#####################################################//
+	
+	int bytes_enviados;  // Quantidade de bytes que foram enviados com sucesso
+	int bytes_recebidos; // Quantidade de bytes que foram recebidos com sucesso
+	
+	int connect_socketfd; //Socket de conexao
+	
+	struct hostent *servidor;         // Struct para ler corretamente de argv[1] atraves de gethostbyname()
+	struct sockaddr_in servidor_addr; // Informacao do endereco do servidor
+  
+	//Caso não haja o nome do servidor, da um erro
+	if (argc != 2) {
+		fprintf(stderr,"uso: nome do servidor\n");
+		exit(1);
+	}
+  
+	servidor = gethostbyname(argv[1]);
+  
+	connect_socketfd = socket(PF_INET, SOCK_STREAM, 0);
+		
+	servidor_addr.sin_family = AF_INET;
+	servidor_addr.sin_port = htons(PORTA_SERVIDOR);  // Porta em "short,network byte order", ver ":$man htons"
+	servidor_addr.sin_addr = *((struct in_addr *)servidor->h_addr); //Endereco IP do servidor
+	memset(&(servidor_addr.sin_zero), '\0', 8); // Por definicao, zerar o vetor sin_zero
+  
+	connect(connect_socketfd, (struct sockaddr *)&servidor_addr, sizeof(struct sockaddr));
+  
+	while(TRUE){
+		bytes_enviados = send(connect_socketfd, mensagem, strlen(mensagem), 0);
+		bytes_recebidos = recv(connect_socketfd, buffer, TAM_BUFFER, 0);
+		if(bytes_recebidos == 0) //Se o servidor encerrou a conexao,sai do loop
+			break;
+		bytes_recebidos = 0;
+		printf("%s\n",buffer);
+	}
+  
+	close(connect_socketfd);
+	
   //#####################################################//
 
-    int bytes_enviados;  // Quantidade de bytes que foram enviados com sucesso
-    int bytes_recebidos; // Quantidade de bytes que foram recebidos com sucesso
-
-    int connect_socketfd; //Socket de conexao
-
-    struct hostent *servidor;         // Struct para ler corretamente de argv[1] atraves de gethostbyname()
-    struct sockaddr_in servidor_addr; // Informacao do endereco do servidor
-    
-    //Caso não haja o nome do servidor, da um erro
-    if (argc != 2) {
-        fprintf(stderr,"uso: nome do servidor\n");
-        exit(1);
-    }
-    
-    servidor = gethostbyname(argv[1]);
-    
-    connect_socketfd = socket(PF_INET, SOCK_STREAM, 0);
-    
-    servidor_addr.sin_family = AF_INET;
-    servidor_addr.sin_port = htons(PORTA_SERVIDOR);  // Porta em "short,network byte order", ver ":$man htons"
-    servidor_addr.sin_addr = *((struct in_addr *)servidor->h_addr); //Endereco IP do servidor
-    memset(&(servidor_addr.sin_zero), '\0', 8); // Por definicao, zerar o vetor sin_zero
-    
-    connect(connect_socketfd, (struct sockaddr *)&servidor_addr, sizeof(struct sockaddr));
-    
-    while(TRUE){
-      bytes_enviados = send(connect_socketfd, mensagem, strlen(mensagem), 0);
-      bytes_recebidos = recv(connect_socketfd, buffer, TAM_BUFFER, 0);
-      if(bytes_recebidos == 0) //Se o servidor encerrou a conexao,sai do loop
-        break;
-      bytes_recebidos = 0;
-      printf("%s\n",buffer);
-    }
-    
-    close(connect_socketfd);
-
-  //#####################################################//
   /*char c;
-
-  c = read_option(); 
-
+		
+		c = read_option(); 
+		
   while(c != SAIR) {
-
-    switch(c) {
-
-    case LISTAR_TODOS_COMPLETO:
-      client_lista_todos_completo();
+	
+	switch(c) {
+	
+	case LISTAR_TODOS_COMPLETO:
+	client_lista_todos_completo();
       break;
     case LISTAR_TODOS:
       client_lista_todos();
