1488958e64ba5fecd3cc75ec7282d1a9a94fbf5f
felipe.eltermann <felipe.eltermann@2732cb02-2d7a-11df-b28a-19025b8c0740>
2010-04-20 15:33:41 +0000

    o mesmo com o servidor. Estamos prontos pra fazer a parte das threads!

--- trunk/proj1/server.c
+++ trunk/proj1/server.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <netdb.h>
 #include "defines.h"
 
 //Bibliotecas para manipulacao de sockets
@@ -11,47 +12,68 @@
 #include <unistd.h>
 
 
-#define QTDE_CONEXOES 10 // número máximo de conexoes pendentes na fila
+#define QTDE_CONEXOES 10 // número máximo de possíveis conexões pendentes na fila
 
 char buffer[TAM_BUFFER];     
 
 char mensagem[TAM_MENSAGEM] = "Servidor diz: Pare de me encher o saco!";
 
-int main()
-{
-    int bytes_enviados;  // Quantidade de bytes que foram enviados com sucesso
-    int bytes_recebidos; // Quantidade de bytes que foram recebidos com sucesso
+int main() {
+	int bytes_enviados;  // Quantidade de bytes que foram enviados com sucesso
+	int bytes_recebidos; // Quantidade de bytes que foram recebidos com sucesso
   
-    int listen_socketfd, connect_socketfd; // Sockets de escuta e de conexao
-    
-    struct sockaddr_in meu_addr;    // Informacoes do meu endereco
-    struct sockaddr_in cliente_addr; // Informacoes do endereco dos clientes
-    int tam_endereco = sizeof(struct sockaddr_in); //Tamanho de um endereco
-    
-    listen_socketfd = socket(PF_INET, SOCK_STREAM, 0); // Cria o socket TCP de escuta
-    printf("Socket TCP de escuta criado!\n");
-    
-    meu_addr.sin_family = AF_INET;
-    meu_addr.sin_port = htons(SERVER_PORT);  // Porta em "short,network byte order", ver ":$man htons"
-    meu_addr.sin_addr.s_addr = INADDR_ANY;  // INADDR_ANY = Descubra o meu endereço IP
-    memset(&(meu_addr.sin_zero), '\0', 8);  // Por definicao, zerar o vetor sin_zero
-    
-    bind(listen_socketfd, (struct sockaddr *)&meu_addr, sizeof(struct sockaddr)); //Atribui a porta utilizada ao socket de escuta
-    printf("Socket atribuido a porta: %d\n",meu_addr.sin_port);
-    
-    listen(listen_socketfd, QTDE_CONEXOES); //Espera por alguem que queira se conectar
-    printf("Esperando alguma conexao...\n");
-    
-    connect_socketfd = accept(listen_socketfd, (struct sockaddr *)&cliente_addr, &tam_endereco); //Aceita a conexao e utiliza o socket para conexoes, deixando o socket de escuta livre
-    printf("Conexao aceita!\n");
-    
-    while(TRUE){
-      bytes_recebidos = recv(connect_socketfd, buffer, TAM_BUFFER, 0);
-      printf("%s\n",buffer);
-      bytes_enviados = send(connect_socketfd, mensagem, strlen(mensagem), 0);
-      close(connect_socketfd);
-      break;
-    }
-    
-    return(0);
+	int listen_socketfd, connect_socketfd; // Sockets de escuta e de conexao
+
+	int status;
+	struct addrinfo opcoes;
+	struct addrinfo *servinfo;  // Informações do meu endereço
+
+	memset(&opcoes, 0, sizeof(opcoes)); // zera a estrutura
+	opcoes.ai_family = AF_INET;         // IPv4
+	opcoes.ai_socktype = SOCK_STREAM;   // TCP stream sockets
+	opcoes.ai_flags = AI_PASSIVE;       // fill in my IP for me
+
+	status = getaddrinfo(NULL, SERVER_PORT_STR, &opcoes, &servinfo);
+	if (status != 0) {
+    fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+    exit(1);
+	}
+
+	// cria o socket TCP de escuta
+	listen_socketfd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
+	printf("Socket TCP de escuta criado!\n");
+  
+  //Atribui a porta utilizada ao socket de escuta
+	status = bind(listen_socketfd, servinfo->ai_addr, servinfo->ai_addrlen); 
+	if (status == -1) {
+    fprintf(stderr, "error on binding the socket to a port\n");
+    exit(1);
+	}
+  
+	//Espera por alguem que queira se conectar
+	listen(listen_socketfd, QTDE_CONEXOES);
+	printf("Esperando alguma conexao...\n");
+  
+	/* Threads Time! */
+	/* TODO */
+	/* loop infinito que aceita as conexões dos clientes */
+
+
+	/* Aceitação de uma única conexão, por enquanto... */
+	struct sockaddr_storage client_addr;
+	socklen_t addr_size;
+
+	// Aceita a conexao e utiliza o socket para conexoes, deixando o socket de escuta livre
+	connect_socketfd = accept(listen_socketfd, (struct sockaddr *)&client_addr, &addr_size);
+	printf("Conexao aceita!\n");
+  
+	while(TRUE){
+		bytes_recebidos = recv(connect_socketfd, buffer, TAM_BUFFER, 0);
+		printf("%s\n",buffer);
+		bytes_enviados = send(connect_socketfd, mensagem, strlen(mensagem), 0);
+		close(connect_socketfd);
+		break;
+	}
+  
+	return(0);
 }
