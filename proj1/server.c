0c38a0f4122f7438b313de3911055f440970d96f
felipe.eltermann <felipe.eltermann@2732cb02-2d7a-11df-b28a-19025b8c0740>
2010-04-22 15:42:40 +0000

    Iniciando parte de threads e outras pequenas alterações

--- proj1/server.c
+++ proj1/server.c
@@ -1,11 +1,14 @@
-//Bibliotecas comuns
+// Bibliotecas comuns
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <netdb.h>
 #include "defines.h"
 
-//Bibliotecas para manipulacao de sockets
+// Biblioteca para threads
+#include <pthread.h>
+
+// Bibliotecas para manipulacao de sockets
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -13,70 +16,98 @@
 
 
 #define QTDE_CONEXOES 10 // número máximo de possíveis conexões pendentes na fila
+#define PTHREAD_THREADS_MAX 10  /* valor maximo de threads simultaneas */
 
 char buffer[TAM_BUFFER];     
 
 char mensagem[TAM_MENSAGEM] = "Servidor diz: Pare de me encher o saco!";
 
+void *trata_conexao (void *socket) {
+  int connect_socketfd;
+  int bytes_recebidos, bytes_enviados;
+  connect_socketfd = (int)socket;
+  printf("Hello World! Agora vou processar a conexão recebida no socket: %d\n", connect_socketfd);
+
+  while(TRUE){
+    bytes_recebidos = recv(connect_socketfd, buffer, TAM_BUFFER, 0);
+    printf("%s\n",buffer);
+    bytes_enviados = send(connect_socketfd, mensagem, strlen(mensagem), 0);
+    close(connect_socketfd);
+    break;
+  }
+
+  printf("Thread diz: acabei a execução.");
+  pthread_exit(NULL);
+}
+
 int main() {
-	int bytes_enviados;  // Quantidade de bytes que foram enviados com sucesso
-	int bytes_recebidos; // Quantidade de bytes que foram recebidos com sucesso
+
+  //  int bytes_enviados;  // Quantidade de bytes que foram enviados com sucesso
+  //int bytes_recebidos; // Quantidade de bytes que foram recebidos com sucesso
   
-	int listen_socketfd, connect_socketfd; // Sockets de escuta e de conexao
+  int listen_socketfd, connect_socketfd; // Sockets de escuta e de conexao
 
-	int status;
-	struct addrinfo opcoes;
-	struct addrinfo *servinfo;  // Informações do meu endereço
+  int status;
+  struct addrinfo opcoes;
+  struct addrinfo *servinfo;  // Informações do meu endereço
 
-	memset(&opcoes, 0, sizeof(opcoes)); // zera a estrutura
-	opcoes.ai_family = AF_INET;         // IPv4
-	opcoes.ai_socktype = SOCK_STREAM;   // TCP stream sockets
-	opcoes.ai_flags = AI_PASSIVE;       // fill in my IP for me
+  memset(&opcoes, 0, sizeof(opcoes)); // zera a estrutura
+  opcoes.ai_family = AF_INET;         // IPv4
+  opcoes.ai_socktype = SOCK_STREAM;   // TCP stream sockets
+  opcoes.ai_flags = AI_PASSIVE;       // fill in my IP for me
 
-	status = getaddrinfo(NULL, SERVER_PORT_STR, &opcoes, &servinfo);
-	if (status != 0) {
+  status = getaddrinfo(NULL, SERVER_PORT_STR, &opcoes, &servinfo);
+  if (status != 0) {
     fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
     exit(1);
-	}
+  }
 
-	// cria o socket TCP de escuta
-	listen_socketfd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
-	printf("Socket TCP de escuta criado!\n");
+  // cria o socket TCP de escuta
+  listen_socketfd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
+  printf("Socket TCP de escuta criado!\n");
   
   //Atribui a porta utilizada ao socket de escuta
-	status = bind(listen_socketfd, servinfo->ai_addr, servinfo->ai_addrlen); 
-	if (status == -1) {
+  status = bind(listen_socketfd, servinfo->ai_addr, servinfo->ai_addrlen); 
+  if (status == -1) {
     fprintf(stderr, "error on binding the socket to a port\n");
     exit(1);
-	}
-	freeaddrinfo(servinfo); // libera a estrutura de informações do servidor
+  }
+  freeaddrinfo(servinfo); // libera a estrutura de informações do servidor
+
+  //Espera por alguem que queira se conectar
+  listen(listen_socketfd, QTDE_CONEXOES);
+  printf("Esperando alguma conexao...\n");
+
+  /* Threads Time! */
+  pthread_t threads[QTDE_CONEXOES]; /* TODO: multi-thread, man! */
+  int i, available_thrs[QTDE_CONEXOES];
+
+  struct sockaddr_storage client_addr;
+  socklen_t addr_size;
+
+  void *exit_status;
+
+  while(TRUE) {
+    /* Aceitação da conexão. */
+    connect_socketfd = accept(listen_socketfd, (struct sockaddr *)&client_addr, &addr_size);
+    if (connect_socketfd == -1){
+      printf("Problema na conexão.\n");
+      continue; /* Desiste dessa conexão e volta a tentar conectar a outro. */
+    }
+    printf("Conexao aceita!\n");
+    
+    /* Inicia uma thread e passa o connect_socketfd pra ela. */
+    printf("Main diz: vou passar para a thread o socket %d.\n", connect_socketfd);
+    /* i = get_av_thr(); */
+    i = 0;
+    pthread_create(&threads[i], NULL, trata_conexao, (void *)connect_socketfd);
+
+    /* Espera a thread terminar */
+    pthread_join(threads[i], &exit_status);
+
+  }
+
 
   
-	//Espera por alguem que queira se conectar
-	listen(listen_socketfd, QTDE_CONEXOES);
-	printf("Esperando alguma conexao...\n");
-  
-	/* Threads Time! */
-	/* TODO */
-	/* loop infinito que aceita as conexões dos clientes */
-
-
-	/* Aceitação de uma única conexão, por enquanto... */
-	struct sockaddr_storage client_addr;
-	socklen_t addr_size;
-
-	// Aceita a conexao e utiliza o socket para conexoes, deixando o socket de escuta livre
-	connect_socketfd = accept(listen_socketfd, (struct sockaddr *)&client_addr, &addr_size);
-	printf("Conexao aceita!\n");
-  
-	while(TRUE){
-		bytes_recebidos = recv(connect_socketfd, buffer, TAM_BUFFER, 0);
-		printf("%s\n",buffer);
-		bytes_enviados = send(connect_socketfd, mensagem, strlen(mensagem), 0);
-		close(connect_socketfd);
-		break;
-	}
-
-  
-	return(0);
+  return(0);
 }
