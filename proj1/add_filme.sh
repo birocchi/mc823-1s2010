c47d635bd6a6fc42f4556b06d13443b61e266912
felipe.eltermann <felipe.eltermann@2732cb02-2d7a-11df-b28a-19025b8c0740>
2010-04-29 01:44:06 +0000

    Caso de uso 6 100%! :)

--- proj1/add_filme.sh
+++ proj1/add_filme.sh
@@ -7,10 +7,10 @@ echo -n "Sinopse: "; read sinopse
 echo -n "Sala: "; read sala
 echo -n "Horários: "; read horarios
 
-tam_substr=$(echo -n $id\@0000\@000000\@$titulo\@$sinopse\@$sala\@$horarios\@ | wc -c)
+tam_substr=$(echo -n $id\@0000\@000\.00\@$titulo\@$sinopse\@$sala\@$horarios\@ | wc -c)
 
 # tam_reg deve ser tam_substr + 3~4 (digitos+@)
-tam_reg=$(echo -n $tam_substr\@$id\@0000\@000000\@$titulo\@$sinopse\@$sala\@$horarios\@ | wc -c)
+tam_reg=$(echo -n $tam_substr\@$id\@0000\@000\.00\@$titulo\@$sinopse\@$sala\@$horarios\@ | wc -c)
 
 # expressão regular ^10*$ casa com qq seq começando com 1 seguida de zeros
 if [ $tam_reg = $(echo $tam_reg | grep ^10*$) ]
@@ -22,6 +22,6 @@ else
 fi
 
 # finalmente, manda os valores para o arquivo
-echo -n $tam_reg\@$id\@0000\@000000\@$titulo\@$sinopse\@$sala\@$horarios\@ >> filmes.dat
+echo -n $tam_reg\@$id\@0000\@000\.00\@$titulo\@$sinopse\@$sala\@$horarios\@ >> filmes.dat
 
 echo "Registro adicionado com sucesso!"
