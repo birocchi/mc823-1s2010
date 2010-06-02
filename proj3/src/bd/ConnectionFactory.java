4c3992598525cf22fdb3047d492977ed96381165
felipe.eltermann <felipe.eltermann@2732cb02-2d7a-11df-b28a-19025b8c0740>
2010-06-02 19:49:54 +0000

    Por algum motivo, é necessário passar o caminho absoluto do arquivo do banco de dados para estabelecer a conexão. Assim, a primeira coisa que o servidor faz é solicitar este caminho.

--- proj3/src/bd/ConnectionFactory.java
+++ proj3/src/bd/ConnectionFactory.java
@@ -6,13 +6,15 @@ import java.sql.SQLException;
 
 public class ConnectionFactory {
 	
+	public static String caminhoAbsolutoBd;
+	
 	public static Connection getConnection() throws SQLException {
 
 		Connection con = null;
 
 		try {
 			Class.forName("org.sqlite.JDBC");
-			con =  DriverManager.getConnection("jdbc:sqlite:./filmes.db");
+			con =  DriverManager.getConnection("jdbc:sqlite:" + caminhoAbsolutoBd);
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
