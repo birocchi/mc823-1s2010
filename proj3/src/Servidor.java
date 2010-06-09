e0f9094a4d9908a491e13ee24d5845553f228ce2
felipe.eltermann <felipe.eltermann@2732cb02-2d7a-11df-b28a-19025b8c0740>
2010-06-09 00:40:04 +0000

    Primeira versão com tudo compilando e rodando na mão. Eclipse só pra editar o código fonte mesmo! :)

--- proj3/src/Servidor.java
+++ proj3/src/Servidor.java
@@ -14,10 +14,10 @@ public class Servidor {
 		// o servidor é executado com o caminho do arquivo do 
 		// BD como primeiro e único parâmetro
 		ConnectionFactory.caminhoAbsolutoBd = args[0];
-		
+
 		ConnectionTest teste = new ConnectionTest();
 		List<Filme> lf = teste.getListaFilmes();
-		
+
 		for (Filme f : lf) {
 			f.printFullInfo();
 			System.out.println("  ---------------------------  ");
