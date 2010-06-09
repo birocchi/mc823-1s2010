837a876ff5f8e5133b3c99381ebb1dc7e7604abd
felipe.eltermann <felipe.eltermann@2732cb02-2d7a-11df-b28a-19025b8c0740>
2010-06-09 19:35:16 +0000

    O nome do arquivo sempre vai ser filmes.db. Assim, não precisa ser passado como parâmetro =P.

--- proj3/runserver.sh
+++ proj3/runserver.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-if [ $# == 0 ]
-then
-    echo "Uso: ./runserver.sh <arquivo do banco>"
-    exit 1
-fi
-
 ROOT=$(pwd) #/home/ec2007/..../mc823-1s2010/proj3
 CLASSPATH=$CLASSPATH:$ROOT/sqlitejdbc-v056.jar
 
 cd bin
 
 # executa o servidor passando como parâmetro o arquivo do BD
-java Servidor $ROOT/$1
+java Servidor $ROOT/filmes.db
