119767a1afdc71b5d080a7d0f382df0fe95920db
anderson.birocchi <anderson.birocchi@2732cb02-2d7a-11df-b28a-19025b8c0740>
2010-06-16 23:35:26 +0000

    Makefile agora monta e limpa arquivos de relatorio, e função que calcula o tempo está funcionando, veja no exemplo como faz, e começo do ajuste do relatório

--- proj3/exemplo/rmiTest/src/RmiClient.java
+++ proj3/exemplo/rmiTest/src/RmiClient.java
@@ -1,6 +1,7 @@
 import java.rmi.*;
 import java.rmi.registry.*;
 import java.net.*;
+import java.util.Calendar;
 
 public class RmiClient {
 
@@ -11,6 +12,7 @@ public class RmiClient {
        String serverAddress=args[0];
        String serverPort=args[1];
        String text=args[2];
+       long t1,t2;
        System.out.println("sending "+text+" to "+serverAddress+":"+serverPort);
        try{
            // get the “registry”
@@ -21,8 +23,23 @@ public class RmiClient {
            // look up the remote object
            rmiServer=
               (ReceiveMessageInterface)(registry.lookup("rmiServer"));
+           
+           //get Calendar instance
+           Calendar now = Calendar.getInstance();
+           /*
+            * To get time in milliseconds, use
+            * long getTimeInMillis() method of Java Calendar class.
+            *
+            * It returns millseconds from Jan 1, 1970.
+            */
+           t1 = System.nanoTime();
+           System.out.println("Inicio: " + t1);
            // call the remote method
            rmiServer.receiveMessage(text);
+           t2 = System.nanoTime();
+           System.out.println("Fim: " + t2);
+           
+           System.out.println("Tempo demorado: " + (t2-t1) + "ns ou " + ((t2-t1)/1000) + "us");
        }
        catch(RemoteException e){
            e.printStackTrace();
